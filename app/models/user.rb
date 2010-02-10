404845b62385180e98e8cb7c4620da033665c6ce
Pavel Krusek <krtz@Pavel-Kruseks-MacBook-Pro.local>
2010-02-10 17:20:43 +0100

    user

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   #acts_as_authentic :login_field_type => 'email', :login_field => 'email'
   acts_as_authentic do |c|
     c.login_field = 'email'
-    c.validates_length_of_password_field_options = { :minimum => 5 }
+    #c.validates_length_of_password_field_options = { :minimum => 5 }
   end
   
   validates_presence_of :first_name, :last_name, :agency
